4db8382ad0595c6d29659408996a9810651647e0
maheshshankhpal_vee24 <mahesh.shankhpal@vee24.com>
2019-04-18 15:49:49 +0530

    first commit

---
+++ SQLDependency_Windows/DB.sql
@@ -0,0 +1,64 @@
+﻿--***************************************************
+--Monitoring Data Change with SqlDependency using C#
+--***************************************************
+
+--## Check Service Broker Enabled / Disbled for database.
+--USE [test]
+--GO
+--SELECT NAME, IS_BROKER_ENABLED FROM SYS.DATABASES
+
+
+--## Enable / Disable Service Broker for existing database.
+--USE [test]
+--GO
+--ALTER DATABASE test SET ENABLE_BROKER
+--ALTER DATABASE test SET DISABLE_BROKER
+
+--## Create sample table in test database
+--USE [test]
+--GO
+--CREATE TABLE [dbo].[Table1](
+--	[SampleId] [bigint] IDENTITY(1,1) NOT NULL,
+--	[SampleName] [nvarchar](50) NOT NULL,
+--	[SampleCategory] [nvarchar](50) NOT NULL,
+--	[SampleDateTime] [datetime] NOT NULL,
+--	[IsSampleProcessed] [bit] NOT NULL)
+-- ON [PRIMARY];
+
+--## Create stored procedure for query notification
+--USE [test]
+--GO
+--CREATE PROCEDURE uspGetSampleInformation
+--AS
+--BEGIN
+--	SELECT
+--		[SampleId],
+--		[SampleName],
+--		[SampleCategory],
+--		[SampleDateTime],
+--		[IsSampleProcessed]
+--	FROM
+--		[dbo].[SampleTable01];
+--END
+
+--## Create QUEUE and SERVICE for the query
+--USE [test]
+--GO
+--CREATE QUEUE QueueSampleInformationDataChange
+--CREATE SERVICE ServiceSampleInformationDataChange
+--	ON QUEUE QueueSampleInformationDataChange
+--	([http://schemas.microsoft.com/SQL/Notifications/PostQueryNotification]);
+
+--GRANT SUBSCRIBE QUERY NOTIFICATIONS TO YourSqlUserName;
+
+--## Give authorization for the Sql user to test database
+--ALTER AUTHORIZATION ON DATABASE::test TO YourSqlUserName;
+
+--## Grand send permission on Service broker service you created for user that will be used to run the client application
+--GRANT SEND ON SERVICE:: ServiceSampleInformationDataChange TO [YourMachineNameOrDomain\UserName];
+
+--## Grand send permission on Service broker queue you created for user that will be used to run the client application
+--GRANT RECEIVE ON dbo.QueueSampleInformationDataChange TO [YourMachineNameOrDomain\UserName];
+
+--## Use this query to see the current queue subscriptions
+--SELECT * FROM SYS.DM_QN_SUBSCRIPTIONS
---
+++ SQLDependency_Windows/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace SQLDependency_Windows
+{
+    public partial class Form1 : Form
+    {
+        private readonly string sampleConnectionString;
+        private SqlDependency sampleSqlDependency;
+        private SqlCommand sampleSqlCommand;
+        private SqlConnection sampleSqlConnection;
+        private readonly string notificationStoredProcedure;
+        public Form1()
+        {
+            this.sampleConnectionString = ConfigurationManager.ConnectionStrings["SampleDbConnection"].ConnectionString;
+            this.notificationStoredProcedure = "uspGetSampleInformation";
+            InitializeComponent();
+        }
+
+
+        private void RegisterDependencyUsingDefaultQueue()
+        {
+            SqlDependency.Stop(this.sampleConnectionString);
+            SqlDependency.Stop(this.sampleConnectionString, "QueueSampleInformationDataChange");
+            SqlDependency.Start(this.sampleConnectionString);
+            {
+                this.ConfigureDependencyUsingStoreProcedureAndDefaultQueue();
+            }
+
+        }
+
+        private async void ConfigureDependencyUsingStoreProcedureAndDefaultQueue()
+        {
+            if (null != this.sampleSqlDependency)
+            {
+                this.sampleSqlDependency.OnChange -= null;
+            }
+
+            if (null != this.sampleSqlCommand)
+            {
+                this.sampleSqlCommand.Dispose();
+            }
+
+            if (null != this.sampleSqlConnection)
+            {
+                this.sampleSqlConnection.Dispose();
+            }
+
+            this.sampleSqlDependency = null;
+            this.sampleSqlCommand = null;
+            this.sampleSqlConnection = null;
+
+            //// Create connection.
+            this.sampleSqlConnection = new SqlConnection(this.sampleConnectionString);
+
+            //// Create command.
+            this.sampleSqlCommand = new SqlCommand { Connection = this.sampleSqlConnection };
+            this.sampleSqlCommand.CommandType = CommandType.StoredProcedure;
+            this.sampleSqlCommand.CommandText = this.notificationStoredProcedure;
+            this.sampleSqlCommand.Notification = null;
+
+            //// Create Sql Dependency.
+            this.sampleSqlDependency = new SqlDependency(this.sampleSqlCommand);
+            this.sampleSqlDependency.OnChange += this.SqlDependencyOnChange;
+            await this.sampleSqlCommand.Connection.OpenAsync();
+            await this.sampleSqlCommand.ExecuteReaderAsync(CommandBehavior.CloseConnection);
+
+            if (null != this.sampleSqlCommand)
+            {
+                this.sampleSqlCommand.Dispose();
+            }
+
+            if (null != this.sampleSqlConnection)
+            {
+                this.sampleSqlConnection.Dispose();
+            }
+        }
+
+        private void SqlDependencyOnChange(object sender, SqlNotificationEventArgs eventArgs)
+        {
+            if (eventArgs.Info == SqlNotificationInfo.Invalid)
+            {
+                //Console.WriteLine("The above notification query is not valid.");
+            }
+            else
+            {
+                loadData();
+            }
+
+            this.ConfigureDependencyUsingStoreProcedureAndDefaultQueue();
+
+        }
+
+        void loadData()
+        {
+            try
+            {
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("select * from Table1", this.sampleConnectionString);
+                DataSet ds = new DataSet();
+                sqlDataAdapter.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            loadData();
+            RegisterDependencyUsingDefaultQueue();
+        }
+
+        
+    }
+
+}
